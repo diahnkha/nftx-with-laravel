8965b187c1faba4fc5cc542e1dd70b2461c31f94
diahnkha <diahnurkhasanah5@gmail.com>
2022-12-05 15:53:00 +0700

    first commit

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,21 @@
+<section id="hero">
+        <div class="container">
+            <div class="content">
+                <p>
+                Using basic skills you can improve your business stuff with Around
+                </p>
+                <h1>
+                    One Dashboard to Manage all Your businesses
+                </h1>
+                <div class="getting-started">
+                    <form action="">
+                        <input type="text" name="Email" id="" placeholder="Input your email...">
+                        <button type="submit">
+                            Try for Free
+                        </button>
+                    </form>
+                </div>
+                <img src="{{ asset('slicing/images/Dashboard.png')}}" alt="">
+            </div>
+        </div>
+    </section>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,29 @@
+<section id="navbar">
+        <div class="container">
+            <div class="logo">
+                <img src="{{ asset('slicing/images/NftiseLogo.png')}}" alt="NftiseLogo">
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li>
+                        <a class="active"href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Feature</a>
+                    </li><li>
+                        <a href="#">Who to Work</a>
+                    </li><li>
+                        <a href="#">Pricing</a>
+                    </li><li>
+                        <a href="#">Reviews</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="button">
+                <a href="">Start Free 14 Trial
+                    <img src="{{ asset('slicing/images/icons/arrow-right.svg')}}" alt="right arrow">
+                </a>
+            </div>
+        </div>
+    </section>
